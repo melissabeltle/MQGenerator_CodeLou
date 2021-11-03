075e6ba5ed85e89c78fa8c58b979930e4daa29f3
melissabeltle <50235978+melissabeltle@users.noreply.github.com>
2021-11-03 10:43:36 -0400

    Restarting Project

---
+++ RabbitMQGenerator/Queue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQGenerator
+{
+    public class Queue
+
+    {
+        string message = "Test Message";
+
+        public string Message { get => message; set => message = value; }
+    }
+
+
+}
+
+    
+
---
+++ RabbitMQGenerator/QueueLibrary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQGenerator
+{
+    public class QueueLibrary
+
+
+    {
+
+        public List<string> QueueNames = new List<string>()
+
+        {
+
+            "COMMS.DCM.CLM.INPUT",
+            "COMMS.DCM.ERA.INPUT",
+            "COMMS.DCM.APLHAII.INPUT",
+            "COMMS.MQSQLIZER.INPUT",
+            "COMMS.CIGNA.CM.OUTPUT",
+            "COMMS.CIGNA.CM.INPUT",
+            "COMMS.CIGNA.ELG.INPUT",
+            "COMMS.CIGNA.ELG.OUTPUT",
+            "COMMS.NDT.CLM.BUILD.INPUT",
+            "FISSBOT.BATCHPARSER.INPUT",
+            "FISSBOT.BATCHROUTER.INPUT"
+
+
+         };
+
+
+
+
+        public string QueueName { get; set; }
+        public string MqMsg { get; set; }
+
+    }
+}
