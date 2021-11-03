af4bb33a85585db7e2e05def608b74d31fe3504a
melissabeltle <50235978+melissabeltle@users.noreply.github.com>
2021-11-03 10:39:54 -0400

    Add project files.

---
+++ RabbitMQGenerator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RabbitMQGenerator
+{
+    static class Program
+    {
+        private static bool message;
+
+        public static bool Message { get => message; set => message = value; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Message);
+        }
+    }
+}
+
+
+
+
+//DoSomethingWithQueue(comboBox1.Text);
+//_queueName = comboBox1.Text;
+
+
+//            ConnecttoRabbit(_queueName);
+
